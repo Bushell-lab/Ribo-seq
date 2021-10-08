2e218adeb1229af1e90c92d71386742c1d087120
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2021-10-08 15:45:10 +0100

    Update and rename RPFs_6_SAM_to_BAM.sh to RPFs_5_SAM_to_BAM.sh

--- Shell_scripts/RPFs_5_SAM_to_BAM.sh
+++ Shell_scripts/RPFs_5_SAM_to_BAM.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-#This script uses SAMtools to convert SAM to BAM format, sort the BAM and make an index.
-#When using the sorted BAM file as input to the counting_script.py script, ensure the corresponding <.bai> (index file) is in the same directory
+#This script uses SAMtools to convert <.sam> files to <.bam> files, sort the <.bam> files and make an <.bai> index files.
+#When using the sorted <.bam> file as input to the counting_script.py script, ensure the corresponding <.bai> (index file) is in the same directory
 
 #read in variables
 source common_variables.sh
 
-#convert SAM to BAM 
-for filename in ${filenames}
+#convert sam to sam 
+for filename in $RPF_filenames
 do
 samtools view -bS $SAM_dir/${filename}_pc_all.sam > $BAM_dir/${filename}_pc_all.bam &
 samtools view -bS $SAM_dir/${filename}_pc_best.sam > $BAM_dir/${filename}_pc_best.bam &
@@ -15,14 +15,14 @@ done
 wait
 
 #sort bam
-for filename in ${filenames}
+for filename in $RPF_filenames
 do
 samtools sort $BAM_dir/${filename}_pc_all.bam -o $BAM_dir/${filename}_pc_all_sorted.bam -@ 6 -m 1G
 samtools sort $BAM_dir/${filename}_pc_best.bam -o $BAM_dir/${filename}_pc_best_sorted.bam -@ 6 -m 1G
 done
 
 #index bam
-for filename in ${filenames}
+for filename in $RPF_filenames
 do
 samtools index $BAM_dir/${filename}_pc_all_sorted.bam $BAM_dir/${filename}_pc_all_sorted.bai &
 samtools index $BAM_dir/${filename}_pc_best_sorted.bam $BAM_dir/${filename}_pc_best_sorted.bai &

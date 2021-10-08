9a62f9c09bcc1c27ad0cb31f1a84f409d18c2102
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2021-10-08 15:53:30 +0100

    Update and rename RPFs_10b_count_counts.sh to RPFs_9b_count_counts.sh

---
+++ Shell_scripts/RPFs_9b_count_counts.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#read in variables
+source common_variables.sh
+
+#run summing_CDS_counts.py script
+#use the best alignments so that all transcripts can be used, without any reads counting twice
+
+for filename in $RPF_filenames
+do
+count_codon_occupancy.py ${filename}_pc_best_final.counts $pc_fasta $region_lengths -in_dir $Count_dir -out_dir $codon_counts_dir &
+done
+wait

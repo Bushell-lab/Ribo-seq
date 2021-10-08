05d6d29440a625457ec42bfb927d6ce91a0a0e59
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2021-10-08 17:06:16 +0100

    Create common_variables.R

---
+++ R_scripts/common_variables.R
@@ -0,0 +1,9 @@
+#Set the parent directory (this should be the same directory as is set in the common_variables.sh script
+parent_dir <- 'Path/to/dir'
+
+#set sample names
+RPF_sample_names <- c('RPF_1', 'RPF_2', 'RPF_3')
+Total_sample_names <- c('Totals_1', 'Totals_2', 'Totals_3')
+
+#set read lengths used for library QC
+lengths <- 25:35

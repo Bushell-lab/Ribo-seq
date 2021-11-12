f4ecf032f619b92d7c9accc05428608c2be5b95a
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2021-11-12 15:53:26 +0000

    Update RPFs_8_Extract_final_counts.sh

--- Shell_scripts/RPFs_8_Extract_final_counts.sh
+++ Shell_scripts/RPFs_8_Extract_final_counts.sh
@@ -10,7 +10,7 @@ lengths='29,30,31,32,33'
 offsets='12,12,13,13,13'
 
 #run counting script to generate <.counts> files for both all and best alignments
-for filename in $filenames
+for filename in $RPF_filenames
 do
 counting_script.py -bam $BAM_dir/${filename}_pc_best_sorted.bam -fasta $pc_fasta -len $lengths -offset $offsets -out_file ${filename}_pc_best_final.counts -out_dir $counts_dir &
 counting_script.py -bam $BAM_dir/${filename}_pc_all_sorted.bam -fasta $pc_fasta -len $lengths -offset $offsets -out_file ${filename}_pc_all_final.counts -out_dir $counts_dir &

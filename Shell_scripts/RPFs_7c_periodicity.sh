857c6f208aec3e4bffcc8f50b4518aaa39e839ac
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-06-01 16:58:27 +0100

    Update RPFs_7c_periodicity.sh

--- Shell_scripts/RPFs_7c_periodicity.sh
+++ Shell_scripts/RPFs_7c_periodicity.sh
@@ -11,7 +11,7 @@ for filename in $RPF_filenames
 do
 for length in $(seq 25 35)
 do
-periodicity.py ${filename}_pc_best_L${length}_Off0.counts $region_lengths -offset $offset -in_dir $counts_dir -out_dir $periodicity_dir &
+periodicity.py ${filename}_pc_L${length}_Off0.counts $region_lengths -offset $offset -in_dir $counts_dir -out_dir $periodicity_dir &
 done
 done
 wait

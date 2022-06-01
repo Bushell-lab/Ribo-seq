17c79545c08b939673ed1c32aeeed868ad6dbc6f
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-06-01 16:57:26 +0100

    Update RPFs_7a_summing_region_counts.sh

--- Shell_scripts/RPFs_7a_summing_region_counts.sh
+++ Shell_scripts/RPFs_7a_summing_region_counts.sh
@@ -11,7 +11,7 @@ for filename in $RPF_filenames
 do
 for length in $(seq 25 35)
 do
-summing_region_counts.py ${filename}_pc_best_L${length}_Off0.counts $offset $region_lengths -in_dir $counts_dir -out_dir $region_counts_dir &
+summing_region_counts.py ${filename}_pc_L${length}_Off0.counts $offset $region_lengths -in_dir $counts_dir -out_dir $region_counts_dir &
 done
 done
 wait

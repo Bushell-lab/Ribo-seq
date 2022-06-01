873295cd9e4213da05336a37f31096ac451fa3e4
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-06-01 16:57:53 +0100

    Update RPFs_7b_summing_spliced_counts.sh

--- Shell_scripts/RPFs_7b_summing_spliced_counts.sh
+++ Shell_scripts/RPFs_7b_summing_spliced_counts.sh
@@ -11,7 +11,7 @@ for filename in $RPF_filenames
 do
 for length in $(seq 25 35)
 do
-summing_spliced_counts.py ${filename}_pc_best_L${length}_Off0.counts $n $region_lengths -in_dir $counts_dir -out_dir $spliced_counts_dir &
+summing_spliced_counts.py ${filename}_pc_L${length}_Off0.counts $n $region_lengths -in_dir $counts_dir -out_dir $spliced_counts_dir &
 done
 done
 wait

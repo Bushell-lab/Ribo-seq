1f58cdd6892b3520e098c76ef9163bdd8e5841b7
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-06-01 17:25:34 +0100

    Update region_counts.R

--- R_scripts/region_counts.R
+++ R_scripts/region_counts.R
@@ -43,10 +43,10 @@ all_data <- do.call("rbind", data_list)
 
 #extract sample and read length from fylenames
 all_data %>%
-  mutate(read_length = str_remove(fyle, ".+pc_best_L"),
+  mutate(read_length = str_remove(fyle, ".+pc_L"),
          read_length = as.numeric(str_remove(read_length, "_Off0_region_counts.csv")),
          sample = str_remove(fyle, ".+region_counts/"),
-         sample = str_remove(sample, "_pc_best.+")) %>%
+         sample = str_remove(sample, "_pc.+")) %>%
   select(-fyle) -> all_data
 
 summary(all_data)

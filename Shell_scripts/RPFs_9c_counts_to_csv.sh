c29a08f4bb4dfcc808dfde73c9b3239b05e5d204
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-06-01 17:19:22 +0100

    Update RPFs_9c_counts_to_csv.sh

--- Shell_scripts/RPFs_9c_counts_to_csv.sh
+++ Shell_scripts/RPFs_9c_counts_to_csv.sh
@@ -3,14 +3,8 @@
 #read in variables
 source common_variables.sh
 
-for filename in $AK_RPF_filenames
+for filename in $RPF_filenames
 do
-counts_to_csv.py ${filename}_pc_final.counts $AK_most_abundant_fasta -all_transcripts -in_dir $counts_dir -out_dir $csv_counts_dir &
-done
-wait
-
-for filename in $APC_RPF_filenames
-do
-counts_to_csv.py ${filename}_pc_final.counts $APC_most_abundant_fasta -all_transcripts -in_dir $counts_dir -out_dir $csv_counts_dir &
+counts_to_csv.py ${filename}_pc_final.counts $most_abundant_fasta -all_transcripts -in_dir $counts_dir -out_dir $csv_counts_dir &
 done
 wait

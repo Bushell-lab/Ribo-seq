da31ceb839a79a9797814f9e5e0ecbbc8238676e
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-06-01 16:55:34 +0100

    Update RPFs_5_SAM_to_BAM.sh

--- Shell_scripts/RPFs_5_SAM_to_BAM.sh
+++ Shell_scripts/RPFs_5_SAM_to_BAM.sh
@@ -9,22 +9,19 @@ source common_variables.sh
 #convert sam to sam 
 for filename in $RPF_filenames
 do
-samtools view -bS $SAM_dir/${filename}_pc_all.sam > $BAM_dir/${filename}_pc_all.bam &
-samtools view -bS $SAM_dir/${filename}_pc_best.sam > $BAM_dir/${filename}_pc_best.bam &
+samtools view -bS $SAM_dir/${filename}_pc.sam > $BAM_dir/${filename}_pc.bam &
 done
 wait
 
 #sort bam
 for filename in $RPF_filenames
 do
-samtools sort $BAM_dir/${filename}_pc_all.bam -o $BAM_dir/${filename}_pc_all_sorted.bam -@ 6 -m 1G
-samtools sort $BAM_dir/${filename}_pc_best.bam -o $BAM_dir/${filename}_pc_best_sorted.bam -@ 6 -m 1G
+samtools sort $BAM_dir/${filename}_pc.bam -o $BAM_dir/${filename}_pc_sorted.bam -@ 6 -m 1G
 done
 
 #index bam
 for filename in $RPF_filenames
 do
-samtools index $BAM_dir/${filename}_pc_all_sorted.bam $BAM_dir/${filename}_pc_all_sorted.bai &
-samtools index $BAM_dir/${filename}_pc_best_sorted.bam $BAM_dir/${filename}_pc_best_sorted.bai &
+samtools index $BAM_dir/${filename}_pc_sorted.bam $BAM_dir/${filename}_pc_sorted.bai &
 done
 wait

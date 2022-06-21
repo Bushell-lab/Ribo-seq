e1692bc40facf1a47822db14153ef5cefd6d8f60
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-06-21 14:46:20 +0100

    Update RPFs_6_Extract_counts_all_lengths.sh

--- Shell_scripts/RPFs_6_Extract_counts_all_lengths.sh
+++ Shell_scripts/RPFs_6_Extract_counts_all_lengths.sh
@@ -3,6 +3,9 @@
 #read in variables
 source common_variables.sh
 
+#make an fai (fasta index) file from the fasta using samtools. This is required for the counting script and needs to exist before running counting_script.py
+samtools faidx $most_abundant_fasta
+
 #run the counting_script.py with a range of read lengths (adjust below if required, currently set to 25-35)
 for filename in $RPF_filenames
 do

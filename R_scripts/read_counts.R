9151b2139ee4af993a47444eb2df013609c361f2
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-06-22 10:35:47 +0100

    Update read_counts.R

--- R_scripts/read_counts.R
+++ R_scripts/read_counts.R
@@ -25,10 +25,12 @@ for (i in 1:nrow(read_counts)) {
   df <- read_counts[i,]
   
   fylename <- df$fylename
-  split_fylename <- str_split(fylename, "_")
+  split_fylename <- str_remove(fylename, ".+\\/")
+  split_fylename <- str_remove(split_fylename, "\\.fastq")
+  split_fylename <- str_split(split_fylename, "_")
   
   fylename_list[[fylename]] <- data.frame(fylename = fylename,
-                            sample = split_fylename[[1]][1],
+                            condition = split_fylename[[1]][1],
                             library = split_fylename[[1]][2],
                             replicate = split_fylename[[1]][3],
                             stage = split_fylename[[1]][4])
@@ -56,7 +58,7 @@ write_csv(file = file.path(parent_dir, "Analysis/reads_summary/Totals_reads_summ
 
 #plot total counts----
 RPF_counts %>%
-  ggplot(aes(x = sample, y = all, fill = replicate))+
+  ggplot(aes(x = condition, y = all, fill = replicate))+
   geom_col(position = position_dodge())+
   ylab("all counts")+
   ggtitle("RPFs counts")+
@@ -67,7 +69,7 @@ print(RPFs_counts_plot)
 dev.off()
 
 Totals_counts %>%
-  ggplot(aes(x = sample, y = all, fill = replicate))+
+  ggplot(aes(x = condition, y = all, fill = replicate))+
   geom_col(position = position_dodge())+
   ylab("all counts")+
   ggtitle("Totals counts")+
@@ -80,7 +82,7 @@ dev.off()
 #plot cutadapt counts %----
 RPF_counts %>%
   mutate(unique_perc = (cutadapt / all) * 100) %>%
-  ggplot(aes(x = sample, y = unique_perc, fill = replicate))+
+  ggplot(aes(x = condition, y = unique_perc, fill = replicate))+
   geom_col(position = position_dodge())+
   ylab("trimmed reads %")+
   ylim(c(0,100))+
@@ -93,7 +95,7 @@ dev.off()
 
 Totals_counts %>%
   mutate(unique_perc = (cutadapt / all) * 100) %>%
-  ggplot(aes(x = sample, y = unique_perc, fill = replicate))+
+  ggplot(aes(x = condition, y = unique_perc, fill = replicate))+
   geom_col(position = position_dodge())+
   ylab("trimmed reads %")+
   ylim(c(0,100))+
@@ -107,7 +109,7 @@ dev.off()
 #plot unique counts %----
 RPF_counts %>%
   mutate(unique_perc = (cdhitdup / cutadapt) * 100) %>%
-  ggplot(aes(x = sample, y = unique_perc, fill = replicate))+
+  ggplot(aes(x = condition, y = unique_perc, fill = replicate))+
   geom_col(position = position_dodge())+
   ylab("Unique reads %")+
   ylim(c(0,100))+
@@ -120,7 +122,7 @@ dev.off()
 
 Totals_counts %>%
   mutate(unique_perc = (cdhitdup / cutadapt) * 100) %>%
-  ggplot(aes(x = sample, y = unique_perc, fill = replicate))+
+  ggplot(aes(x = condition, y = unique_perc, fill = replicate))+
   geom_col(position = position_dodge())+
   ylab("Unique reads %")+
   ylim(c(0,100))+
@@ -134,7 +136,7 @@ dev.off()
 #rRNA %----
 RPF_counts %>%
   mutate(rRNA_perc = (rRNA / cdhitdup) * 100) %>%
-  ggplot(aes(x = sample, y = rRNA_perc, fill = replicate))+
+  ggplot(aes(x = condition, y = rRNA_perc, fill = replicate))+
   geom_col(position = position_dodge())+
   ylab("rRNA reads %")+
   ylim(c(0,100))+
@@ -147,7 +149,7 @@ dev.off()
 
 Totals_counts %>%
   mutate(rRNA_perc = (rRNA / cdhitdup) * 100) %>%
-  ggplot(aes(x = sample, y = rRNA_perc, fill = replicate))+
+  ggplot(aes(x = condition, y = rRNA_perc, fill = replicate))+
   geom_col(position = position_dodge())+
   ylab("rRNA reads %")+
   ylim(c(0,100))+
@@ -165,11 +167,11 @@ RPF_counts %>%
          tRNA_perc = (tRNA / non_rRNA_counts) * 100,
          mito_perc = (mito / non_rRNA_counts) * 100,
          unaligned_perc = (unaligned / non_rRNA_counts) * 100,
-         sample_name = paste(sample, replicate, sep = " ")) %>%
-  select(sample_name, pc_perc, tRNA_perc, mito_perc, unaligned_perc) %>%
+         sample = paste(condition, replicate, sep = " ")) %>%
+  select(sample, pc_perc, tRNA_perc, mito_perc, unaligned_perc) %>%
   gather(key = alignment, value = percentage, pc_perc, tRNA_perc, mito_perc, unaligned_perc) %>%
   mutate(alignment = factor(alignment, levels = c("unaligned_perc", "mito_perc", "tRNA_perc", "pc_perc"), labels = c("un-aligned", "mito", "tRNA", "pc"), ordered = T)) %>%
-  ggplot(aes(x = sample_name, y = percentage, fill = alignment))+
+  ggplot(aes(x = sample, y = percentage, fill = alignment))+
   geom_col()+
   xlab("sample")+
   ylab("% aligments")+

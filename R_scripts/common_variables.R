09b6866ac1612f84e2c91731275eb358b98efcb1
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-09-08 10:46:22 +0100

    Update common_variables.R

--- R_scripts/common_variables.R
+++ R_scripts/common_variables.R
@@ -2,8 +2,16 @@
 parent_dir <- 'Path/to/dir'
 
 #set sample names
-RPF_sample_names <- c('RPF_1', 'RPF_2', 'RPF_3')
-Total_sample_names <- c('Totals_1', 'Totals_2', 'Totals_3')
+RPF_sample_names <- c('Ctrl_RPFs_1', 'Ctrl_RPFs_2', 'Ctrl_RPFs_3', 'Treatment_RPFs_1', 'Treatment_RPFs_2', 'Treatment_RPFs_3')
+Total_sample_names <- c('Ctrl_Totals_1', 'Ctrl_Totals_2', 'Ctrl_Totals_3', 'Treatment_Totals_1', 'Treatment_Totals_2', 'Treatment_Totals_3')
+
+RPF_sample_info <- data.frame(sample = RPF_sample_names,
+                             condition = c(rep("Ctrl", 3), rep("Treatment", 3)),
+                             replicate = rep(c("1", "2", "3"), 2))
+
+Total_sample_info <- data.frame(sample = Total_sample_names,
+                             condition = c(rep("Ctrl", 3), rep("Treatment", 3)),
+                             replicate = rep(c("1", "2", "3"), 2))
 
 #set read lengths used for library QC
 lengths <- 25:35

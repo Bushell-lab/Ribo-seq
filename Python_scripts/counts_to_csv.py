2e30441c8e8d7bed81f3d043bcee1c693a94cbec
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-09-08 17:23:24 +0100

    Update counts_to_csv.py

--- Python_scripts/counts_to_csv.py
+++ Python_scripts/counts_to_csv.py
@@ -33,12 +33,24 @@ def batch_write_out_csv(sequence_dict,values_dict,name_suffix):
                 for line in lines:
                     g.write(','.join([str(x) for x in line])+'\n')
 
+def write_out_one_csv(counts_dict, sequences, out_name):
+    '''batch writes a lot of csv'''
+    with open(out_name,'w') as g:
+        g.write(','.join(['transcript', 'Position', 'Nucleotide', 'Counts'])+'\n')
+        for transcript in counts_dict.keys():
+            counts, sequence = counts_dict[transcript], sequences[transcript]
+            if len(counts) == len(sequence):
+                lines = zip([transcript] * len(counts), range(1,len(counts)+1), sequence, counts)
+                for line in lines:
+                    g.write(','.join([str(x) for x in line])+'\n')
+
 def main():
     parser = argparse.ArgumentParser(description='Reformats <.counts> file to <.csv> file')
     parser.add_argument('counts_fyle',type=str,help='<.counts> file to pull values from')
     parser.add_argument('fasta',type=str,help='<.fasta> file used to generate the <.counts>')
     parser.add_argument('-transcript',default=None,type=str,help='Specific transcript to reformat')
     parser.add_argument('-all_transcripts',action='store_true',help='Reformat all, output to new directory')
+    parser.add_argument('-one_csv',action='store_true',help='Reformat all, output to one csv file')
     parser.add_argument('-in_dir', type=str, default=None, help='Change the input folder, default is current directory')
     parser.add_argument('-out_dir', type=str, default=None, help='Change the output folder, default is current directory')
     args = parser.parse_args()
@@ -53,7 +65,7 @@ def main():
     #read in fasta sequences
     sequences = read_in_fasta(args.fasta)
     
-    if args.transcript and not args.all_transcripts:
+    if args.transcript and not args.all_transcripts and not args.one_csv:
         #Check Values
         seq = sequences[args.transcript] if args.transcript in sequences else None
         counts = counts_dict[args.transcript] if args.transcript in counts_dict else None
@@ -73,7 +85,7 @@ def main():
         else:
             print('One or more of the files did not contain entry {}'.format(args.transcript))
     
-    elif args.all_transcripts and not args.transcript:
+    elif args.all_transcripts and not args.transcript and not args.one_csv:
         #Directory
         if args.out_dir == None:
             new_dir = args.counts_fyle.replace('.counts','_all_csvs')
@@ -89,8 +101,19 @@ def main():
         else:
             print('{} folder already exists. Remove or rename and try again.'.format(new_dir))
     
+    elif args.one_csv and not args.transcript and not args.all_transcripts:
+        #create out filename
+        if args.out_dir == None:
+            out_name = args.counts_fyle.replace('.counts','_counts.csv')
+        else:
+            out_name = args.out_dir + '/' + args.counts_fyle.replace('.counts','_counts.csv')
+        
+        #write csv
+        write_out_one_csv(counts_dict, sequences, out_name)
+    
+    
     else:
-        print('Check options, use only one of -all_transcripts and -transcript')
+        print('Check options, use only one of -all_transcripts,  -transcript, -one_csv')
 
 if __name__ == '__main__':
     main()

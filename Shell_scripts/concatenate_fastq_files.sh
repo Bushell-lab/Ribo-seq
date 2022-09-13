878a9c22ab7f8a185e181aee5f8c802d9a447ad8
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-09-13 15:34:31 +0100

    Update concatenate_fastq_files.sh

--- Shell_scripts/concatenate_fastq_files.sh
+++ Shell_scripts/concatenate_fastq_files.sh
@@ -4,7 +4,7 @@
 source common_variables.sh
 
 #concatenate seperate fastq files into one
-cat ${fastq_files}/RPFs_1a.fastq ${fastq_files}/RPFs_1b.fastq > ${fastq_files}/RPFs_1.fastq
-cat ${fastq_files}/RPFs_2a.fastq ${fastq_files}/RPFs_2b.fastq > ${fastq_files}/RPFs_2.fastq
+cat ${fastq_dir}/RPFs_1a.fastq ${fastq_dir}/RPFs_1b.fastq > ${fastq_dir}/RPFs_1.fastq
+cat ${fastq_dir}/RPFs_2a.fastq ${fastq_dir}/RPFs_2b.fastq > ${fastq_dir}/RPFs_2.fastq
 
 

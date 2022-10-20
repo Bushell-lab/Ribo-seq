cff7048fbe16b070fee3fcba10dbbab0676dbdf7
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-10-20 15:21:16 +0100

    Delete fastq_lines.sh
    Read counts extracted from log files instead

--- Shell_scripts/fastq_lines.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#This script will calculate the lines in every fastq file (lines /4 = reads)
-
-#read in variables
-source common_variables.sh
-
-wc -l $fastq_dir/*.fastq > $log_dir/fastq_lines.txt

af6d0a4692215a72bb058ee8c21f294bf883b08d
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-10-24 10:55:22 +0100

    Update Totals_4a_align_reads_rsem.sh
    Updated include UMI extracted  fastq files. Also no longer aligns to rRNA with bbmap first

--- Shell_scripts/Totals_4a_align_reads_rsem.sh
+++ Shell_scripts/Totals_4a_align_reads_rsem.sh
@@ -1,28 +1,20 @@
 #!/usr/bin/env bash
 
-#This script first uses bbmap to map to and remove reads corresponding to rRNAs, tRNAs and mitocondrial mRNAs.
-#in specificies the input <.fastq> file.
-#out specificies the output <.SAM> file.
-#ref specifies the <.fasta> file to use as a reference. bbmap will use this to make an index. As this is much quicker than other alignment programs, we use the nodisk option so that this isn't written to file
-#outm and outu specificies filenames to write <.fastq> files for all reads that either align or do not align respectively
-#ambigous specifies how to treat multimapped reads. 
-#2> stores the text that is printed to the screen as a log
-
-#RSEM is then used to map the non rRNA/tRNA/mito mRNA reads to the same protein coding transcriptome as used for the RPFs.
+#This script uses RSEM to map reads to the protein coding transcriptome. It is strongly recommended to use the filtered transcriptome unless there is a good reason otherwise
+#--strandedness describes the strand of the genome that the sequencing reads should align to. For the CORALL kit this is forward, but for a lot of standed Illumina Tru-seq kits this will be reverse. If this is not known then it is best to try both and the alignment logs should tell you which is correct
+#--bowtie2 sets bowtie2 as the aligner
+#--fragment-length-mean 300 --fragment-length-sd 100 sets the mean and standard deviation of the library fragment size. These do not need to be exact but best estimates. These are good starting values to use for the CORALL kit
+#if reads are paired end, then the following line needs to be added and also inputs for --fragment-length-mean and --fragment-length-sd don't need to be set as these will be calculated from the data
+#--paired-end $fastq_dir/${filename}_UMI_clipped_R1.fastq $fastq_dir/${filename}_UMI_clipped_R2.fastq
+#Note that the options for extracting UMIs with UMI tools in the previous script will also have had to been altered for paired end reads
 
 #read in variables
 source common_variables.sh
 
-#Align to rRNA
+#Align to protein coding transcriptome
 for filename in $Totals_filenames
 do
-bbmap.sh in=$fastq_dir/${filename}_UMIremoved.fastq out=$SAM_dir/${filename}_rRNA.sam ref=$rRNA_fasta outm=$fastq_dir/${filename}_rRNA.fastq outu=$fastq_dir/${filename}_non_rRNA.fastq ambiguous=best nodisk threads=16 2> $log_dir/${filename}_rRNA_log.txt
-done
-
-#Align to protein coding transcriptomes
-for filename in $Totals_filenames
-do
-rsem-calculate-expression --strandedness forward --bowtie2 --fragment-length-mean 300 --fragment-length-sd 100 $fastq_dir/${filename}_non_rRNA.fastq $rsem_index $rsem_dir/${filename} &
+rsem-calculate-expression --strandedness forward --bowtie2 --fragment-length-mean 300 --fragment-length-sd 100 $fastq_dir/${filename}_UMI_clipped.fastq $rsem_index $rsem_dir/${filename} &
 done
 wait
 

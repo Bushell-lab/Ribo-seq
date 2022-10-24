91aad524e8e07bc8fb69ce7346795ec0c6188663
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-10-24 12:10:28 +0100

    Update Totals_3a_align_reads_transcriptome.sh
    outputs a sorted and indexed BAM file rather than a SAM file

--- Shell_scripts/Totals_3a_align_reads_transcriptome.sh
+++ Shell_scripts/Totals_3a_align_reads_transcriptome.sh
@@ -2,7 +2,7 @@
 
 #This script uses bbmap to align reads.
 #in specificies the input <.fastq> file.
-#out specificies the output <.SAM> file.
+#bamscript=bs.sh; sh bs.sh uses samtools to output a sorted and indexed <.bam> file rather than <.sam>
 #ref specifies the <.fasta> file to use as a reference. bbmap will use this to make an index. As this is much quicker than other alignment programs, we use the nodisk option so that this isn't written to file
 #ambigous specifies how to treat multimapped reads. We use all so that RSEM can calculated relative isoform expression downstream
 #2> stores the text that is printed to the screen as a log
@@ -13,5 +13,5 @@ source common_variables.sh
 #Align to protein coding transcriptome
 for filename in $Totals_filenames
 do
-bbmap.sh in=$fastq_dir/${filename}_UMI_clipped.fastq out=$SAM_dir/${filename}_pc.sam ref=$pc_fasta ambiguous=all nodisk threads=$threadN 2> $log_dir/${filename}_pc_log.txt
+bbmap.sh in=$fastq_dir/${filename}_UMI_clipped.fastq out=$SAM_dir/${filename}_pc.sam bamscript=bs.sh; sh bs.sh ref=$pc_fasta ambiguous=all nodisk threads=$threadN 2> $log_dir/${filename}_pc_log.txt
 done

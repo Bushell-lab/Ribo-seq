115da0b94373dd11482af776b24205710a3c3b79
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-10-25 14:31:54 +0100

    Update Totals_3b_align_reads_genome.sh
    Changed STAR option to output unsorted BAM file as sorted option is giving an error

--- Shell_scripts/Totals_3b_align_reads_genome.sh
+++ Shell_scripts/Totals_3b_align_reads_genome.sh
@@ -8,8 +8,10 @@
 #"--outFilterMismatchNmax" sets the maximum number of mismatches (default is 10, but we set to 5 to be more stringent)
 #"--outSAMprimaryFlag AllBestScore" will output all alignments with the best score as primary alignments, rather than default behaviour which will only mark one alignment as primary
 #"--alignEndsType EndToEnd" forces end-to-end read alignment, do not soft-clip
-#"--outSAMtype BAM SortedByCoordinate" outputs a sorted <.bam> file, circumventing the requirement to run samtools afterwards
+#"--outSAMtype BAM Unsorted" outputs an unsorted <.bam> file
 #"--outSAMunmapped None" means unmapped reads are not output (set as default)
+
+#samtools then sorts and indexes the <.bam> file
 
 #read in variables
 source common_variables.sh
@@ -17,5 +19,18 @@ source common_variables.sh
 #Align to genome
 for filename in $Totals_filenames
 do
-STAR --readFilesIn $fastq_dir/${filename}_UMI_clipped.fastq --runThreadN $threadN --genomeDir $STAR_index --outFilterMultimapNmax 5 --outFilterMismatchNmax 5 --outSAMprimaryFlag AllBestScore --alignEndsType EndToEnd --outSAMtype BAM SortedByCoordinate --outSAMunmapped None --sjdbGTFfile $STAR_GTF --outFileNamePrefix $STAR_dir/${filename}
+STAR --readFilesIn $fastq_dir/${filename}_UMI_clipped.fastq --runThreadN $threadN --genomeDir $STAR_index --outFilterMultimapNmax 5 --outFilterMismatchNmax 5 --outSAMprimaryFlag AllBestScore --alignEndsType EndToEnd --outSAMtype BAM Unsorted --outSAMunmapped None --sjdbGTFfile $STAR_GTF --outFileNamePrefix $STAR_dir/${filename}
 done
+
+#sort bam
+for filename in $Totals_filenames
+do
+samtools sort $STAR_dir/${filename}Aligned.out.bam -o $BAM_dir/${filename}_genome_sorted.bam -@ $threadN -m 1G
+done
+
+#index bam
+for filename in $Totals_filenames
+do
+samtools index $BAM_dir/${filename}_genome_sorted.bam $BAM_dir/${filename}_genome_sorted.bai &
+done
+wait

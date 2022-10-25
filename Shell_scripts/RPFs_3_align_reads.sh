500edc1fb7452f0e9c53e02e69eba8b60904aa49
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-10-25 13:19:55 +0100

    Update RPFs_3_align_reads.sh
    changed so that samtools sorts and indexes bam file after aligning, as doing this as part of bbmap was throwing an error

--- Shell_scripts/RPFs_3_align_reads.sh
+++ Shell_scripts/RPFs_3_align_reads.sh
@@ -9,8 +9,10 @@
 #2> stores the text that is printed to the screen as a log
 #We first align to rRNA and then tRNA, spliting the reads into aligned and unaligned <.fastq> files (without outputting a <.sam> file).
 #All non-rRNA and non-tRNA reads are then aligned to a protein coding transcriptome (prefarably a <.fasta> containing the most abundant transcripts created in the total RNA-seq pipeline).
-#bamscript=bs.sh; sh bs.sh uses samtools to output a sorted and indexed <.bam> file rather than <.sam>
+
 #fastQC is then ran on all the output <.fastq> files
+
+#samtools is then used to sort and index the <.bam> file
 
 #read in variables
 source common_variables.sh
@@ -30,8 +32,21 @@ done
 #Align to protein coding transcriptome
 for filename in $RPF_filenames
 do
-bbmap.sh in=$fastq_dir/${filename}_non_rRNA_tRNA.fastq out=$SAM_dir/${filename}_pc.sam bamscript=bs.sh; sh bs.sh ref=$most_abundant_fasta outm=$fastq_dir/${filename}_pc.fastq outu=$fastq_dir/${filename}_unaligned.fastq ambiguous=best nodisk threads=$threadN 2> $log_dir/${filename}_pc_log.txt
+bbmap.sh in=$fastq_dir/${filename}_non_rRNA_tRNA.fastq out=$BAM_dir/${filename}_pc.bam ref=$most_abundant_fasta outm=$fastq_dir/${filename}_pc.fastq outu=$fastq_dir/${filename}_unaligned.fastq ambiguous=best nodisk threads=$threadN 2> $log_dir/${filename}_pc_log.txt
 done
+
+#sort bam
+for filename in $RPF_filenames
+do
+samtools sort $BAM_dir/${filename}_pc.bam -o $BAM_dir/${filename}_pc_sorted.bam -@ $threadN -m 1G
+done
+
+#index bam
+for filename in $RPF_filenames
+do
+samtools index $BAM_dir/${filename}_pc_sorted.bam $BAM_dir/${filename}_pc_sorted.bai &
+done
+wait
 
 #run fastqc on mapped reads
 for filename in $RPF_filenames

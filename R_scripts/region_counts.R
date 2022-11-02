1deb938495d6ef8e4179dd2559d9e5a2dbe1409d
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2022-11-02 09:27:09 +0000

    Update region_counts.R
    Updated and added code to plot all percentage counts on one plot

--- R_scripts/region_counts.R
+++ R_scripts/region_counts.R
@@ -7,6 +7,9 @@ library(parallel)
 #read in common variables
 source("common_variables.R")
 
+#set the read lengths you wish to plot
+lengths <- 25:35
+
 #functions
 #write a function that will read in a csv file for use with parLapply
 read_counts_csv <- function(k){
@@ -39,72 +42,119 @@ data_list <- parLapply(cl, fyle_list, read_counts_csv) #reads in the data
 stopCluster(cl) #Stops cluster
 
 #combine data_list into one data frame
-all_data <- do.call("rbind", data_list)
-
-#extract sample and read length from fylenames
-all_data %>%
+#extract sample and read length from fyle and inner_join with sample info to get condition and replicate
+do.call("rbind", data_list) %>%
   mutate(read_length = str_remove(fyle, ".+pc_L"),
          read_length = as.numeric(str_remove(read_length, "_Off0_region_counts.csv")),
          sample = str_remove(fyle, ".+region_counts/"),
          sample = factor(str_remove(sample, "_pc.+"))) %>%
+  inner_join(RPF_sample_info, by = "sample") %>%
   select(-fyle) -> all_data
 
 summary(all_data)
 
 #plot data----
 #plot length distribution of all counts
+#summed counts
 all_data %>%
-  group_by(sample, read_length) %>%
-  summarise(total_counts = sum(counts, na.rm = T)) -> total_counts
-
-total_counts %>%
-  mutate(sample = factor(sample, levels = RPF_sample_names, labels = str_replace_all(RPF_sample_names, "_", " "))) %>%
-  ggplot(aes(x = read_length, y = total_counts, colour = sample))+
+  group_by(condition, replicate, read_length) %>%
+  summarise(read_length_counts = sum(counts, na.rm = T)) %>%
+  ggplot(aes(x = read_length, y = read_length_counts, colour = condition, lty = replicate, shape = replicate))+
   geom_line(size = 1)+
+  geom_point(size = 2)+
   myTheme+
   ylab("total counts")+
   xlab("read length")+
   scale_x_continuous(breaks = seq(min(lengths), max(lengths),2)) -> total_counts_plot
 
-png(filename = file.path(parent_dir, "plots/summed_counts/all_counts_lengths.png"), width = 500, height = 300)
+png(filename = file.path(parent_dir, "plots/summed_counts/lengths_count_plot.png"), width = 500, height = 300)
 print(total_counts_plot)
 dev.off()
 
+#percentage counts
+all_data %>%
+  group_by(condition, replicate) %>%
+  summarise(total_counts = sum(counts, na.rm = T)) -> total_counts
+
+all_data %>%
+  group_by(condition, replicate, read_length) %>%
+  summarise(read_length_counts = sum(counts, na.rm = T)) %>%
+  inner_join(total_counts, by = c("condition", "replicate")) %>%
+  mutate(perc_counts = (read_length_counts / total_counts) * 100) %>%
+  ggplot(aes(x = read_length, y = perc_counts, colour = condition, lty = replicate, shape = replicate))+
+  geom_line(size = 1)+
+  geom_point(size = 2)+
+  myTheme+
+  ylab("% counts")+
+  xlab("read length")+
+  scale_x_continuous(breaks = seq(min(lengths), max(lengths),2)) -> perc_counts_plot
+
+png(filename = file.path(parent_dir, "plots/summed_counts/lengths_perc_plot.png"), width = 500, height = 300)
+print(perc_counts_plot)
+dev.off()
+
 #for each sample, plot summed counts within each region across the length distribution
-plot_list <- list()
 for (sample in RPF_sample_names) {
   all_data[all_data$sample == sample,] %>%
     mutate(region = factor(region, levels = c("UTR3", "UTR5", "CDS"), labels = c("3\'UTR", "5\'UTR", "CDS"), ordered = T)) %>%
-    group_by(sample, read_length, region) %>%
-    summarise(total_counts = sum(counts, na.rm = T)) %>%
-    ggplot(aes(x = read_length, y = total_counts, fill = region))+
+    group_by(read_length, region) %>%
+    summarise(region_counts = sum(counts, na.rm = T)) %>%
+    ggplot(aes(x = read_length, y = region_counts, fill = region))+
     geom_col()+
     myTheme+
     xlab("read length")+
     ylab("total counts")+
-    ggtitle(str_replace_all(sample, "_", " ")) -> plot_list[[sample]]
+    scale_x_continuous(breaks = seq(min(lengths), max(lengths),2))+
+    ggtitle(str_replace_all(sample, "_", " ")) -> region_counts_plot
   
   png(filename = file.path(parent_dir, paste0("plots/summed_counts/", sample, "_summed_region_counts.png")), width = 500, height = 300)
-  print(plot_list[[sample]])
+  print(region_counts_plot)
   dev.off()
 }
 
 #for each sample, plot percentage counts within each region across the length distribution
-plot_list <- list()
+all_data %>%
+  group_by(sample, read_length) %>%
+  summarise(total_counts = sum(counts, na.rm = T)) -> read_length_counts
+
 for (sample in RPF_sample_names) {
   all_data[all_data$sample == sample,] %>%
-    inner_join(total_counts, by = c("sample", "read_length")) %>%
-    mutate(perc_counts = (counts / total_counts * 100),
+    group_by(sample, region, read_length) %>%
+    summarise(region_counts = sum(counts, na.rm = T)) %>%
+    ungroup() %>%
+    inner_join(read_length_counts, by = c("sample", "read_length")) %>%
+    mutate(perc_counts = (region_counts / total_counts * 100),
            region = factor(region, levels = c("UTR3", "UTR5", "CDS"), labels = c("3\'UTR", "5\'UTR", "CDS"), ordered = T)) %>%
+    #summary()
     ggplot(aes(x = read_length, y = perc_counts, fill = region))+
     geom_col()+
     myTheme+
     xlab("read length")+
     ylab("% counts")+
-    ggtitle(str_replace_all(sample, "_", " ")) -> plot_list[[sample]]
+    scale_x_continuous(breaks = seq(min(lengths), max(lengths),2))+
+    ggtitle(str_replace_all(sample, "_", " ")) -> region_perc_plot
   
   png(filename = file.path(parent_dir, paste0("plots/summed_counts/", sample, "_perc_region_counts.png")), width = 500, height = 300)
-  print(plot_list[[sample]])
+  print(region_perc_plot)
   dev.off()
 }
 
+#plot region percentages for all samples in one plot
+all_data %>%
+  group_by(sample, region, read_length) %>%
+  summarise(region_counts = sum(counts, na.rm = T)) %>%
+  ungroup() %>%
+  inner_join(read_length_counts, by = c("sample", "read_length")) %>%
+  mutate(perc_counts = (region_counts / total_counts * 100),
+         region = factor(region, levels = c("UTR3", "UTR5", "CDS"), labels = c("3\'UTR", "5\'UTR", "CDS"), ordered = T)) %>%
+  ggplot(aes(x = read_length, y = perc_counts, colour = region))+
+  geom_point()+
+  #geom_boxplot(aes(fill = region))+
+  myTheme+
+  xlab("read length")+
+  ylab("% counts")+
+  scale_x_continuous(breaks = seq(min(lengths), max(lengths),2)) -> region_perc_plot
+
+png(filename = file.path(parent_dir, "plots/summed_counts/all_samples_perc_region_counts.png"), width = 500, height = 300)
+print(region_perc_plot)
+dev.off()

8e425f027e49a1f6133fc6b8d497b1c2c9f8c1fe
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2023-03-10 15:00:51 +0000

    Update RPFs_3_align_reads.sh
    Added the trimreaddescription=t to the protein coding alignment section, which has been found to be important for using UMItools downstream if any samples have come from more than one sequencing run

--- Shell_scripts/RPFs_3_align_reads.sh
+++ Shell_scripts/RPFs_3_align_reads.sh
@@ -6,6 +6,7 @@
 #ref specifies the <.fasta> file to use as a reference. bbmap will use this to make an index. As this is much quicker than other alignment programs, we use the nodisk option so that this isn't written to file
 #outm and outu specificies filenames to write <.fastq> files for all reads that either align or do not align respectively
 #ambigous specifies how to treat multimapped reads. We use best (keeps the highest scored alignment).
+#trimreaddescription=t removes any white space and following text from the fastq files when writing the <.bam> files. This is important for using UMItools downstream if any samples have come from more than one sequencing run
 #2> stores the text that is printed to the screen as a log
 #We first align to rRNA and then tRNA, spliting the reads into aligned and unaligned <.fastq> files (without outputting a <.sam> file).
 #All non-rRNA and non-tRNA reads are then aligned to a protein coding transcriptome (prefarably a <.fasta> containing the most abundant transcripts created in the total RNA-seq pipeline).
@@ -32,7 +33,7 @@ done
 #Align to protein coding transcriptome
 for filename in $RPF_filenames
 do
-bbmap.sh in=$fastq_dir/${filename}_non_rRNA_tRNA.fastq out=$BAM_dir/${filename}_pc.bam ref=$most_abundant_fasta outm=$fastq_dir/${filename}_pc.fastq outu=$fastq_dir/${filename}_unaligned.fastq ambiguous=best nodisk threads=$threadN 2> $log_dir/${filename}_pc_log.txt
+bbmap.sh in=$fastq_dir/${filename}_non_rRNA_tRNA.fastq out=$BAM_dir/${filename}_pc.bam ref=$most_abundant_fasta outm=$fastq_dir/${filename}_pc.fastq outu=$fastq_dir/${filename}_unaligned.fastq ambiguous=best nodisk trimreaddescription=t threads=$threadN 2> $log_dir/${filename}_pc_log.txt
 done
 
 #sort bam

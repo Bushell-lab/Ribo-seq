0d98e7560cff99441bc124a12537625f34e9c3f1
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2023-07-31 14:24:37 +0100

    Update Totals_6b_extract_read_counts.sh

--- Shell_scripts/Totals_6b_extract_read_counts.sh
+++ Shell_scripts/Totals_6b_extract_read_counts.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+#This script extracts the read counts at each stage of the pipeline from the log files. It is paired with the Totals_read_counts.R, which will will then make plots for this QC.
+
 #read in variables
 source common_variables.sh
 
-#monosomes
+#Extract the read counts from the log files for each sample
 for filename in $Totals_filenames
 do
 extract_read_counts.py ${filename} Totals -log_dir $log_dir &

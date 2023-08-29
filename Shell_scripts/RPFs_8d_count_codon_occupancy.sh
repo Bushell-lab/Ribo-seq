fbfdb0160688dbcc8a1dedd8d1ccaf1138dec5f5
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2023-08-29 13:22:02 +0100

    Update RPFs_8d_count_codon_occupancy.sh

--- Shell_scripts/RPFs_8d_count_codon_occupancy.sh
+++ Shell_scripts/RPFs_8d_count_codon_occupancy.sh
@@ -6,6 +6,6 @@ source common_variables.sh
 #run count_codon_occupancy.py to calculate codon occupancy counts in each position of the ribosome
 for filename in $RPF_filenames
 do
-count_codon_occupancy.py ${filename}_pc_final_codon.counts $most_abundant_fasta $region_lengths -in_dir $counts_dir -out_dir $codon_counts_dir &
+count_codon_occupancy.py ${filename}_pc_final.counts $most_abundant_fasta $region_lengths -in_dir $counts_dir -out_dir $codon_counts_dir &
 done
 wait

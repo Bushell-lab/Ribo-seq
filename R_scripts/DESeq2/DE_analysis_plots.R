1f7a2d389df554a22b56a9b654861efd33057182
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2023-11-10 15:55:48 +0000

    Update DE_analysis_plots.R

--- R_scripts/DESeq2/DE_analysis_plots.R
+++ R_scripts/DESeq2/DE_analysis_plots.R
@@ -45,7 +45,7 @@ totals %>%
   mytheme+
   xlab("log2FC")+
   ylab("-log10(padj)")+
-  ggtitle(paste(treatment, "Totals")) -> totals_volcano
+  ggtitle(paste(treatment, "Cytoplasmic RNA")) -> totals_volcano
 
 png(filename = file.path(parent_dir, "plots/DE_analysis", paste0("Totals_", treatment, "_volcano.png")), width = 300, height = 400)
 print(totals_volcano)
@@ -76,7 +76,7 @@ totals %>%
   mytheme+
   xlab("log10(mean expression)")+
   ylab("log2FC")+
-  ggtitle(paste(treatment, "Totals")) -> totals_MA
+  ggtitle(paste(treatment, "Cytoplasmic RNA")) -> totals_MA
 
 png(filename = file.path(parent_dir, "plots/DE_analysis", paste0("Totals_", treatment, "_MA.png")), width = 400, height = 300)
 print(totals_MA)
@@ -105,56 +105,69 @@ RPFs %>%
                                      TE_padj < TE_sig_padj & TE_log2FC > 0 ~ "TE up",
                                      TE_padj >= TE_non_sig_padj ~ "no change",
                                      (TE_padj >= TE_sig_padj & TE_padj <= TE_non_sig_padj) | is.na(TE_padj) ~ "NS"),
-                           levels = c("TE down", "no change", "TE up", "NS"), ordered = T),
-         RPFs_group = factor(case_when(RPFs_padj < RPF_sig_padj & RPFs_log2FC < 0 & totals_padj >= RPF_non_sig_padj ~ "RPFs down",
-                                  RPFs_padj < RPF_sig_padj & RPFs_log2FC > 0 & totals_padj >= RPF_non_sig_padj ~ "RPFs up",
-                                  RPFs_padj >= RPF_non_sig_padj & totals_padj < RPF_sig_padj & totals_log2FC < 0 ~"Totals down",
-                                  RPFs_padj >= RPF_non_sig_padj & totals_padj < RPF_sig_padj & totals_log2FC > 0 ~"Totals up",
-                                  RPFs_padj < RPF_sig_padj & totals_padj < RPF_sig_padj & RPFs_log2FC < 0 & totals_log2FC < 0 ~ "both down",
-                                  RPFs_padj < RPF_sig_padj & totals_padj < RPF_sig_padj & RPFs_log2FC > 0 & totals_log2FC > 0 ~ "both up"))) -> merged_data
+                           levels = c("TE down", "TE up", "no change", "NS"), ordered = T),
+         RPFs_group = factor(case_when((RPFs_padj < RPF_sig_padj & RPFs_log2FC < -log2FC_threshold) & (totals_padj >= RPF_non_sig_padj | totals_log2FC > log2FC_threshold) ~ "RPFs down",
+                                       (RPFs_padj < RPF_sig_padj & RPFs_log2FC > log2FC_threshold) & (totals_padj >= RPF_non_sig_padj | totals_log2FC < -log2FC_threshold)  ~ "RPFs up",
+                                       (totals_padj < RPF_sig_padj & totals_log2FC < -log2FC_threshold) & (RPFs_padj >= RPF_non_sig_padj | RPFs_log2FC > log2FC_threshold)  ~ "Totals down",
+                                       (totals_padj < RPF_sig_padj & totals_log2FC > log2FC_threshold) & (RPFs_padj >= RPF_non_sig_padj | RPFs_log2FC < -log2FC_threshold)  ~ "Totals up",
+                                       RPFs_padj < RPF_sig_padj & totals_padj < RPF_sig_padj & RPFs_log2FC < -log2FC_threshold & totals_log2FC < -log2FC_threshold ~ "both down",
+                                       RPFs_padj < RPF_sig_padj & totals_padj < RPF_sig_padj & RPFs_log2FC > log2FC_threshold & totals_log2FC > log2FC_threshold ~ "both up",
+                                       totals_padj >= RPF_non_sig_padj & RPFs_padj >= RPF_non_sig_padj ~ "no change")),
+         RPFs_group = factor(case_when(is.na(RPFs_group) ~ "NS",
+                                       !(is.na(RPFs_group)) ~ RPFs_group), levels = c("RPFs down", "RPFs up", "Totals down", "Totals up", "both down", "both up", "no change", "NS"), ordered = T)) -> merged_data
 summary(merged_data)
 
 
 #plot TE scatters----
 #based on RPFs/totals logFC
+#add "n=" labels
 merged_data %>%
-  mutate(alpha_score = case_when(is.na(RPFs_group) ~ 0.1,
-                                 !(is.na(RPFs_group)) ~ 1)) %>%
-  ggplot(aes(x = totals_log2FC, y = RPFs_log2FC, colour = RPFs_group, alpha = alpha_score))+
+  group_by(RPFs_group) %>%
+  summarize(num = n()) %>%
+  mutate(lab = paste0(RPFs_group, " (n=", num, ") ")) -> labs
+
+merged_data %>%
+  mutate(alpha_score = case_when(RPFs_group == "NS" | RPFs_group == "no change" ~ 0.1,
+                                 RPFs_group != "NS" & RPFs_group != "no change" ~ 1)) %>%
+  arrange(desc(RPFs_group)) %>%
+  left_join(labs, by = "RPFs_group") %>%
+  ggplot(aes(x = totals_log2FC, y = RPFs_log2FC, colour = lab, alpha = alpha_score))+
   geom_point()+
   scale_alpha(guide = "none")+
+  scale_color_manual(values=c("#A6CEE3", "#1F78B4", "#2e4057", "grey", "#FB9A99", "#33A02C", "#B2DF8A", "#E31A1C"))+
   mytheme+
-  xlab("Total RNA log2FC")+
+  xlab("Cytoplasmic RNA log2FC")+
   ylab("RPFs log2FC")+
   ggtitle(paste(treatment, "TE scatter"))+
   xlim(c(-5,5))+
   ylim(c(-5,5))+
   geom_abline(lty=1)+
   geom_hline(yintercept = 0, lty=1)+
-  geom_hline(yintercept = 1, lty=2)+
-  geom_hline(yintercept = -1, lty=2)+
-  geom_vline(xintercept = 0, lty=1)+
-  geom_vline(xintercept = 1, lty=2)+
-  geom_vline(xintercept = -1, lty=2) -> RPF_groups_scatter_plot
+  geom_vline(xintercept = 0, lty=1) -> RPF_groups_scatter_plot
 
 png(filename = file.path(parent_dir, "plots/DE_analysis", paste(treatment, "_RPF_groups_scatter.png")), width = 500, height = 400)
 print(RPF_groups_scatter_plot)
 dev.off()
 
-#write out group sizes
-write.table(file = file.path(parent_dir, "plots/DE_analysis", paste0(treatment, "_RPF_groups_scatter.txt")), summary(merged_data$RPFs_group), col.names = F, quote = F)
-
 #based on TE
+#add "n=" labels
+merged_data %>%
+  group_by(TE_group) %>%
+  summarize(num = n()) %>%
+  mutate(lab = paste0(TE_group, "\n(n=", num, ")\n")) -> labs
+
 merged_data %>%
   filter(!(is.na(TE_group))) %>%
-  mutate(alpha_score = case_when(TE_group =="TE down" | TE_group =="TE up" | TE_group == "no change" ~ 1,
-                                 TE_group =="NS" ~ 0.1)) %>%
-  ggplot(aes(x = totals_log2FC, y = RPFs_log2FC, colour = TE_group, alpha = alpha_score))+
+  arrange(desc(TE_group)) %>%
+  mutate(alpha_score = case_when(TE_group =="TE down" | TE_group =="TE up" ~ 1,
+                                 TE_group == "no change" | TE_group =="NS" ~ 0.1)) %>%
+  left_join(labs, by = "TE_group") %>%
+  ggplot(aes(x = totals_log2FC, y = RPFs_log2FC, colour = lab, alpha = alpha_score))+
   geom_point()+
-  scale_colour_manual(values=c("red", "blue", "purple", "grey"))+
+  scale_colour_manual(values=c("#2e4057", "grey", "#d1495b", "purple"))+
   scale_alpha(guide = "none")+
   mytheme+
-  xlab("Total RNA log2FC")+
+  xlab("Cytoplasmic RNA log2FC")+
   ylab("RPFs log2FC")+
   ggtitle(paste(treatment, "TE scatter"))+
   xlim(c(-5,5))+
@@ -170,9 +183,6 @@ merged_data %>%
 png(filename = file.path(parent_dir, "plots/DE_analysis", paste(treatment, "_TE_scatter.png")), width = 500, height = 400)
 print(TE_scatter_plot)
 dev.off()
-
-#write out group sizes
-write.table(file = file.path(parent_dir, "plots/DE_analysis", paste0(treatment, "_TE_scatter_groups.txt")), summary(merged_data$TE_group), col.names = F, quote = F)
 
 #write out csv
 write_csv(merged_data, file.path(parent_dir, "Analysis/DESeq2_output/merged_DESeq2.csv"))

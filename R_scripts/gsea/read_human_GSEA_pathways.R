4c33903d45cf23117fd5f474bd247a9923911fc1
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2025-09-01 13:00:28 +0100

    Update read_human_GSEA_pathways.R

--- R_scripts/gsea/read_human_GSEA_pathways.R
+++ R_scripts/gsea/read_human_GSEA_pathways.R
@@ -1,9 +1,10 @@
 #set the path to the gsea gmt files (need to be downloaded first from https://www.gsea-msigdb.org/gsea/msigdb/human/collections.jsp)
-gsea_dir <- ""
+#Check this script is in the same directory as the .gmt files below, or add path accordingly
 
-pathways.hallmark <- gmtPathways(file.path(gsea_dir, "h.all.v7.1.symbols.gmt"))
-pathways.bio_processes <- gmtPathways(file.path(gsea_dir, "c5.bp.v7.1.symbols.gmt"))
-pathways.mol_funs <- gmtPathways(file.path(gsea_dir, "c5.mf.v7.1.symbols.gmt"))
-pathways.cell_comp <- gmtPathways(file.path(gsea_dir, "c5.cc.v7.1.symbols.gmt"))
-pathways.kegg <- gmtPathways(file.path(gsea_dir, "c2.cp.kegg.v7.1.symbols.gmt"))
+pathways.hallmark <- gmtPathways("h.all.v7.1.symbols.gmt")
+pathways.bio_processes <- gmtPathways("c5.bp.v7.1.symbols.gmt")
+pathways.mol_funs <- gmtPathways("c5.mf.v7.1.symbols.gmt")
+pathways.cell_comp <- gmtPathways("c5.cc.v7.1.symbols.gmt")
+pathways.kegg <- gmtPathways("c2.cp.kegg.v7.1.symbols.gmt")
+
 

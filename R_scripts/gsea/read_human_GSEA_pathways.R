2dd08848206e25fa86f0e9baca6f04a190d6c045
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2025-09-01 13:02:06 +0100

    Update read_human_GSEA_pathways.R

--- R_scripts/gsea/read_human_GSEA_pathways.R
+++ R_scripts/gsea/read_human_GSEA_pathways.R
@@ -1,10 +1,10 @@
-#set the path to the gsea gmt files (need to be downloaded first from https://www.gsea-msigdb.org/gsea/msigdb/human/collections.jsp)
-#Check this script is in the same directory as the .gmt files below, or add path accordingly
+#Check this script is in the same directory as the .gmt files below, or add path accordingly (need to be downloaded first from https://www.gsea-msigdb.org/gsea/msigdb/mouse/collections.jsp)
 
 pathways.hallmark <- gmtPathways("h.all.v7.1.symbols.gmt")
 pathways.bio_processes <- gmtPathways("c5.bp.v7.1.symbols.gmt")
 pathways.mol_funs <- gmtPathways("c5.mf.v7.1.symbols.gmt")
 pathways.cell_comp <- gmtPathways("c5.cc.v7.1.symbols.gmt")
 pathways.kegg <- gmtPathways("c2.cp.kegg.v7.1.symbols.gmt")
+
 
 

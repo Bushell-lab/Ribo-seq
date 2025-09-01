c5289dea18944cfb0019e4d46898d584cedd1b40
Joseph-Waldron <40466320+Joseph-Waldron@users.noreply.github.com>
2025-09-01 13:01:46 +0100

    Update read_mouse_GSEA_pathways.R

--- R_scripts/gsea/read_mouse_GSEA_pathways.R
+++ R_scripts/gsea/read_mouse_GSEA_pathways.R
@@ -1,8 +1,8 @@
-#set the path to the gsea gmt files (need to be downloaded first from https://www.gsea-msigdb.org/gsea/msigdb/mouse/collections.jsp)
-gsea_dir <- ""
+#Check this script is in the same directory as the .gmt files below, or add path accordingly (need to be downloaded first from https://www.gsea-msigdb.org/gsea/msigdb/mouse/collections.jsp)
 
-pathways.hallmark <- gmtPathways(file.path(gsea_dir, "mh.all.v2022.1.Mm.symbols.gmt"))
-pathways.bio_processes <- gmtPathways(file.path(gsea_dir, "m5.go.bp.v2022.1.Mm.symbols.gmt"))
-pathways.mol_funs <- gmtPathways(file.path(gsea_dir, "m5.go.mf.v2022.1.Mm.symbols.gmt"))
-pathways.cell_comp <- gmtPathways(file.path(gsea_dir, "m5.go.cc.v2022.1.Mm.symbols.gmt"))
-pathways.curated <- gmtPathways(file.path(gsea_dir, "m2.all.v2022.1.Mm.symbols.gmt"))
+pathways.hallmark <- gmtPathways("mh.all.v2022.1.Mm.symbols.gmt")
+pathways.bio_processes <- gmtPathways("m5.go.bp.v2022.1.Mm.symbols.gmt")
+pathways.mol_funs <- gmtPathways("m5.go.mf.v2022.1.Mm.symbols.gmt")
+pathways.cell_comp <- gmtPathways("m5.go.cc.v2022.1.Mm.symbols.gmt")
+pathways.curated <- gmtPathways("m2.all.v2022.1.Mm.symbols.gmt")
+
